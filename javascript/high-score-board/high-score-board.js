df4967df749253f2a4b07dfaedf1dfaf7b0ccbdb
albertonaval <albertonaval.dev@gmail.com>
2023-03-01 19:02:08 +0100

    Done exercise

--- javascript/high-score-board/high-score-board.js
+++ javascript/high-score-board/high-score-board.js
@@ -7,14 +7,10 @@
  * @returns {Record<string, number>} new score board
  */
 export function createScoreBoard() {
-
   let scoreBoard = {
     'The Best Ever': 1000000
   }
-
   return scoreBoard
-
-  throw new Error('Please implement the createScoreBoard function');
 }
 
 /**
@@ -26,9 +22,8 @@ export function createScoreBoard() {
  * @returns {Record<string, number>} updated score board
  */
 export function addPlayer(scoreBoard, player, score) {
-
-
-  throw new Error('Please implement the addPlayer function');
+  scoreBoard[player] = score
+  return scoreBoard
 }
 
 /**
@@ -39,7 +34,8 @@ export function addPlayer(scoreBoard, player, score) {
  * @returns {Record<string, number>} updated score board
  */
 export function removePlayer(scoreBoard, player) {
-  throw new Error('Please implement the removePlayer function');
+  delete scoreBoard[player]
+  return scoreBoard
 }
 
 /**
@@ -51,7 +47,9 @@ export function removePlayer(scoreBoard, player) {
  * @returns {Record<string, number>} updated score board
  */
 export function updateScore(scoreBoard, player, points) {
-  throw new Error('Please implement the updateScore function');
+  scoreBoard[player] += points
+  return scoreBoard
+
 }
 
 /**
@@ -61,7 +59,10 @@ export function updateScore(scoreBoard, player, points) {
  * @returns {Record<string, number>} updated score board
  */
 export function applyMondayBonus(scoreBoard) {
-  throw new Error('Please implement the applyMondayBonus function');
+  for (const player in scoreBoard) {
+    scoreBoard[player] += 100
+  }
+  return scoreBoard
 }
 
 /**
@@ -71,5 +72,5 @@ export function applyMondayBonus(scoreBoard) {
  * @returns {number} normalized score
  */
 export function normalizeScore(params) {
-  throw new Error('Please implement the normalizeScore function');
+  return params.normalizeFunction(params.score)
 }
